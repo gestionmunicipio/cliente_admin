0f63109ca686b4f97840b94a0262d0a2ed1538b9
gestionmunicipio <86022128+gestionmunicipio@users.noreply.github.com>
2025-05-31 19:14:52 -0300

    Archivos de despliegue para Render

--- cliente_admin_django/settings.py
+++ cliente_admin_django/settings.py
@@ -118,11 +118,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+#--- STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'clientes/static'),
-]
+#---STATICFILES_DIRS = [
+#---    os.path.join(BASE_DIR, 'clientes/static'),
+#---]
 
 
 # Default primary key field type
