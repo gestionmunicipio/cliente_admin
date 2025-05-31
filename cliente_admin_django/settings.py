c7a7454325bc8cb901b6f4eb7fe79099455c36f0
gestionmunicipio <86022128+gestionmunicipio@users.noreply.github.com>
2025-05-30 22:54:16 -0300

    Fix: eliminado duplicados de logo y limpiado staticfiles

--- cliente_admin_django/settings.py
+++ cliente_admin_django/settings.py
@@ -117,7 +117,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'clientes/static'),
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
@@ -129,4 +135,4 @@ LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/login/'
 
 ALLOWED_HOSTS = ['cliente-admin.onrender.com']
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
