8df8e6fa863d9ec8f82b8cbfb809c7574ff2bda9
gestionmunicipio <86022128+gestionmunicipio@users.noreply.github.com>
2025-06-22 02:08:01 -0300

    Update settings.py

--- cliente_admin/settings.py
+++ cliente_admin/settings.py
@@ -134,7 +134,6 @@ STATICFILES_DIRS = [
 
 LOCALE_PATHS = [os.path.join(BASE_DIR, 'locale')]
 
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
